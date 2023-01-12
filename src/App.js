79eb4143b7061d8f29e2500867017431672b07e7
sagar-ksahoo <sagarsahoo1729@gmail.com>
2023-01-12 18:15:34 +0530

    programs card added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Hero from "./components/Hero";
+import Programs from "./components/Programs";
 
 function App() {
   return (
     <div className="App">
       <Hero />
+      <Programs />
     </div>
   );
 }
---
+++ src/components/Programs.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Programs.css";
+import { programsData } from "../data/programsData";
+import RightArrow from "../images/rightArrow.png";
+const Programs = () => {
+  return (
+    <>
+      <div className="programs">
+        <div className="programs-header">
+          <span className="stroke-text">Explore Our</span>
+          <span>Programs</span>
+          <span className="stroke-text">to Shape You</span>
+        </div>
+
+        <div className="programs-categories">
+          {programsData.map((program) => (
+            <div className="category">
+              {program.image}
+              <span>{program.heading}</span>
+              <span>{program.details}</span>
+              <div className="join-now">
+                <span>Join Now</span>
+                <img src={RightArrow} alt="RightArrow" />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Programs;
