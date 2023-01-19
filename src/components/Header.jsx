69b8414f9c6cbafd26df16f445bb93bcc6333dce
sagar-ksahoo <sagarsahoo1729@gmail.com>
2023-01-19 16:21:40 +0530

    mobile responsive added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import Logo from "../images/logo.png";
+import Bars from "../images/bars.png";
 
 const Header = () => {
+  const mobile = window.innerWidth <= 768 ? true : false;
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <>
       <div className="header">
         <img src={Logo} alt="Company Logo" className="logo" />
-
-        <ul className="header-menu">
-          <li>Home</li>
-          <li>Programs</li>
-          <li>Why Us</li>
-          <li>Our Plans</li>
-          <li>Testimonials</li>
-        </ul>
+        {menuOpen === false && mobile ? (
+          <div
+            style={{
+              backgroundColor: "var(--appColor)",
+              padding: "0.5rem",
+              borderRadius: "5px",
+            }}
+            onClick={() => setMenuOpen(true)}
+          >
+            <img
+              src={Bars}
+              alt="bars-image"
+              style={{ width: "1.5rem", height: "1.5rem" }}
+            />
+          </div>
+        ) : (
+          <ul className="header-menu">
+            <li onClick={() => setMenuOpen(false)}>Home</li>
+            <li onClick={() => setMenuOpen(false)}>Programs</li>
+            <li onClick={() => setMenuOpen(false)}>Why Us</li>
+            <li onClick={() => setMenuOpen(false)}>Our Plans</li>
+            <li onClick={() => setMenuOpen(false)}>Testimonials</li>
+          </ul>
+        )}
       </div>
     </>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,6 +10,8 @@ import { motion } from "framer-motion";
 const Hero = () => {
   const transition = { type: "spring", duration: 3 };
 
+  const mobile = window.innerWidth <= 768 ? true : false;
+
   return (
     <>
       <div className="hero">
@@ -20,7 +22,7 @@ const Hero = () => {
           {/* The Tagline for your fitness brand*/}
           <div className="the-best-ad">
             <motion.div
-              initial={{ left: "238px" }}
+              initial={{ left: mobile ? "150px" : "238px" }}
               whileInView={{ left: "8px" }}
               transition={{ ...transition, type: "tween" }}
             ></motion.div>
